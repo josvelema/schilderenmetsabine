bc51b9b227169888af35d74467e258668d6c29cd
josvelema <volumensolutions@gmail.com>
2025-05-16 15:17:29 +0200

    troubleshooting server 2

--- schilderen_site/wsgi.py
+++ schilderen_site/wsgi.py
@@ -13,4 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "schilderen_site.settings.production")
 
+print("DJANGO_SETTINGS_MODULE =", os.environ.get("DJANGO_SETTINGS_MODULE"))  # tijdelijk toevoegen voor debug
+
+
 application = get_wsgi_application()
