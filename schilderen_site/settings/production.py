5e765dd17b807e18f2385583dffda84a7abb4acd
josvelema <volumensolutions@gmail.com>
2025-05-16 14:46:24 +0200

    allowd hosts production.py

--- schilderen_site/settings/production.py
+++ schilderen_site/settings/production.py
@@ -2,6 +2,14 @@ from .base import *
 
 DEBUG = False
 
+ALLOWED_HOSTS = [
+    'schilderen-met-sabine.onrender.com',
+    'www.schilderen-met-sabine.onrender.com',
+    'schilderenmetsabine.nl',
+    'www.schilderenmetsabine.nl',
+]
+
+
 try:
     from .local import *
 except ImportError:
