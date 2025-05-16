f043deaf6d7980e9c9dc21e429328c7c6c35a837
josvelema <volumensolutions@gmail.com>
2025-05-16 17:16:26 +0200

    troubleshooting server 3

--- schilderen_site/settings/production.py
+++ schilderen_site/settings/production.py
@@ -1,8 +1,12 @@
 from .base import *
 
+print("✅ Using production settings")
+print("✅ ROOT_URLCONF is:", ROOT_URLCONF)
+
+
 DEBUG = False
 
-ALLOWED_HOSTS = [
+ALLOWED_HOSTS = ['.onrender.com',            
     'schilderen-met-sabine.onrender.com',
     'www.schilderen-met-sabine.onrender.com',
     'schilderenmetsabine.nl',
