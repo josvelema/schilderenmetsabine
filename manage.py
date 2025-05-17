fa34519172ada98425431e1f8a8427f027e97548
josvelema <volumensolutions@gmail.com>
2025-05-17 21:22:50 +0200

    temp user adding script 2.1

--- manage.py
+++ manage.py
@@ -3,10 +3,10 @@ import os
 import sys
 
 if __name__ == "__main__":
-    import create_admin_user
     # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "schilderen_site.settings.production")
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "schilderen_site.settings.dev")
 
     from django.core.management import execute_from_command_line
+    import create_admin_user
 
     execute_from_command_line(sys.argv)
