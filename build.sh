dd1666c493840016dfd85351d8564b3b6996f32c
josvelema <volumensolutions@gmail.com>
2025-05-17 21:29:31 +0200

    temp user adding script 3

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 pip install -r requirements.txt
+echo "▶️ Applying migrations..."
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
+echo "▶️ Creating superuser if needed..."
+python manage.py shell < create_admin_user.py
