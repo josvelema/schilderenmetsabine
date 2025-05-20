b7dbed9ccde06e7f0a78d1178767de553130e37e
josvelema <volumensolutions@gmail.com>
2025-05-20 12:56:31 +0200

    create user 2 requirements prod

--- manage.py
+++ manage.py
@@ -3,8 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "schilderen_site.settings.production")
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "schilderen_site.settings.dev")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "schilderen_site.settings.production")
+    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "schilderen_site.settings.dev")
 
     from django.core.management import execute_from_command_line
     import create_admin_user
