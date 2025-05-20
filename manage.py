c034bab917d3288301675f4a44dbdf6666de6ffd
josvelema <volumensolutions@gmail.com>
2025-05-20 12:44:08 +0200

    create user

--- manage.py
+++ manage.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "schilderen_site.settings.dev")
 
     from django.core.management import execute_from_command_line
-    # import create_admin_user
+    import create_admin_user
 
     execute_from_command_line(sys.argv)
