beca610dfe13db0f6166bbdf55727bbe0f1fbf94
josvelema <volumensolutions@gmail.com>
2025-05-20 12:48:59 +0200

    create user 2

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
-pip install -r requirements.txt
-echo "▶️ Applying migrations..."
+echo "▶️ Collecting static files..."
+python manage.py collectstatic --noinput
+
 echo "▶️ Making migrations..."
 python manage.py makemigrations --noinput
+
+echo "▶️ Applying migrations..."
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+
 echo "▶️ Creating superuser if needed..."
 python manage.py shell < create_admin_user.py
