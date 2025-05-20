4bb11a5bcebd2062b5eeeb0d9a6007bebb495884
josvelema <volumensolutions@gmail.com>
2025-05-20 11:40:57 +0200

    workshop page styling and whatsapp widget

--- build.sh
+++ build.sh
@@ -1,13 +1,9 @@
-#!/bin/bash
-
-echo "▶️ Collecting static files..."
-python manage.py collectstatic --noinput
-
+#!/usr/bin/env bash
+pip install -r requirements.txt
+echo "▶️ Applying migrations..."
 echo "▶️ Making migrations..."
 python manage.py makemigrations --noinput
-
-echo "▶️ Applying migrations..."
 python manage.py migrate --noinput
-
+python manage.py collectstatic --noinput
 echo "▶️ Creating superuser if needed..."
-python manage.py shell < create_admin_user.py
+python manage.py shell < create_admin_user.pypytho
